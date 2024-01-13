ec0898fc85037d55cdbf4edf56154a9077f44585
LimHyeji <ihj028003@gmail.com>
2024-01-13 16:09:03 +0900

    Feat: 수학 풀이

---
+++ csharp_algo/csharp_algo/baekjoon/boj_2407_한수.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+namespace csharp_algo.baekjoon
+{
+    class boj_2407_한수
+    {
+        static int n, m;
+
+        static void Program(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            n = int.Parse(input[0]);
+            m = int.Parse(input[1]);
+
+            BigInteger result = solution(n, m);
+
+            Console.WriteLine(result);
+        }
+
+        static BigInteger solution(int n, int m)
+        {
+            BigInteger a = 1, b = 1;
+
+            for (int i = n - m + 1; i <= n; i++) a *= i;
+            for (int i = 1; i <= m; i++) b *= i;
+
+            return a / b;
+        }
+    }
+}
