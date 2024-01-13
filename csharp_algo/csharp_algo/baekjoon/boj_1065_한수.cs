9083cbb24428a985fce19df2939cb3a55c328d0a
LimHyeji <ihj028003@gmail.com>
2024-01-13 16:10:08 +0900

    Feat: 완전탐색 풀이

---
+++ csharp_algo/csharp_algo/baekjoon/boj_1065_한수.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace csharp_algo.baekjoon
+{
+    class boj_1065_한수
+    {
+        static int N;
+        static int result;
+
+        internal static void Main(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+
+            if (N < 100)
+            {
+                Console.WriteLine(N);
+                return;
+            }
+
+            result = 99;
+            for (int i = 100; i <= N; i++)
+            {
+                char[] number = i.ToString().ToCharArray();
+
+                int diff = number[1] - number[0];
+                Boolean flag = true;
+                for (int j = 2; j < number.Length; j++)
+                {
+                    if (number[j] - number[j - 1] != diff)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) result++;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
