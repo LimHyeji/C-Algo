96eac659df40ec8468a43f191f8ea657b3aaa458
LimHyeji <ihj028003@gmail.com>
2024-01-18 22:17:23 +0900

    Feat: 파싱 풀이

---
+++ csharp_algo/csharp_algo/programmers/하샤드수.cs
@@ -0,0 +1,20 @@
+﻿namespace csharp_algo.programmers
+{
+    class 하샤드수
+    {
+        public bool solution(int x)
+        {
+            bool answer = true;
+
+            string number = x.ToString();
+
+            int sum = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum += number[i] - '0';
+            }
+
+            return x % sum == 0 ? true : false;
+        }
+    }
+}
