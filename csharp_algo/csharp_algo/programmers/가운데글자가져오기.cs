286a13ecc02137bf69ee9ddb4ab5cb25b463d39f
hyyej_ji <87571953+LimHyeji@users.noreply.github.com>
2024-01-19 21:04:58 +0900

    Feat: 문자열 풀이

---
+++ csharp_algo/csharp_algo/programmers/가운데글자가져오기.cs
@@ -0,0 +1,18 @@
+﻿namespace csharp_algo.programmers
+{
+    class 가운데글자가져오기
+    {
+          public string solution(string s) {
+          int len = s.Length;
+          
+          if(len % 2 == 0){
+              int mid = len/2;   
+              return s.Substring(mid-1,2);
+          }
+          else{
+              int mid = len/2;
+              return s.Substring(mid,1);
+          }
+      }
+    }
+}
