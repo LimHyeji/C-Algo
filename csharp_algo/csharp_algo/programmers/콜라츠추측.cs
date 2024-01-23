c06de8ffbf19aca1bcdd295d36d5180a7741346c
hyyej_ji <87571953+LimHyeji@users.noreply.github.com>
2024-01-23 20:38:30 +0900

    Feat: 구현 풀이

---
+++ csharp_algo/csharp_algo/programmers/콜라츠추측.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp_algo.programmers
+{
+    class 가운데글자가져오기
+    {
+          public int solution(int num) {
+        
+            if(num == 1) return 0;
+            
+            int answer = 0;
+            
+            long number = num;
+            while(number != 1){
+                long temp = number%2;
+                
+                if(temp == 0) number /= 2;
+                else number = number*3 + 1;
+                
+                answer++;
+                
+                if(answer == 500) return -1;
+            }
+            
+            return answer;
+        }
+    }
+}
