0950f19b76511fc4436cb5e879829fd7c9c52b74
hyyej_ji <87571953+LimHyeji@users.noreply.github.com>
2024-01-24 17:46:51 +0900

    Feat: 그리디 풀이

---
+++ csharp_algo/csharp_algo/baekjoon/boj_1931_회의실배정.cs
@@ -0,0 +1,86 @@
+using System;
+using static System.Console;
+/*
+11
+1 4
+3 5
+0 6
+5 7
+3 8
+5 9
+6 10
+8 11
+8 12
+2 13
+12 14
+ 
+ (1,4), (5,7), (8,11), (12,14) 
+ */
+namespace csharp_algo.baekjoon
+{
+    internal class boj_1931_회의실배정
+    {
+        static int N;
+
+        class Room
+        {
+            public int start;
+            public int end;
+
+            public Room(int start, int end)
+            {
+                this.start = start;
+                this.end = end;
+            }
+        }
+        static Room[] rooms;
+        static int result;
+
+        static public void Main(string[] args)
+        {
+            N = int.Parse(ReadLine());
+            rooms = new Room[N];
+
+            for(int i=0; i<N; i++)
+            {
+                string[] input = ReadLine().Split(' ');
+                int start = int.Parse(input[0]);
+                int end = int.Parse(input[1]);
+                rooms[i]= new Room(start, end);
+            }
+
+            result = 1;
+
+            Array.Sort(rooms, delegate(Room o1, Room o2) { 
+                if(o1.end==o2.end) return o1.start.CompareTo(o2.start);
+                return o1.end.CompareTo(o2.end); 
+            });
+
+            int idx = 0;
+            while (idx < N)
+            {
+                Room cur = rooms[idx];
+                // WriteLine(cur.start);
+
+                bool flag = false;
+                for(int i=idx+1; i<N; i++)
+                {
+                    if (cur.end <= rooms[i].start)
+                    {
+                        // WriteLine(cur+ " :: " + rooms[i].start);
+                        idx=i;
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (flag) result++;
+                else break;
+            }
+
+            WriteLine(result);
+
+        }
+
+    }
+}
