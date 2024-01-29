9fb7c4c079df9b64c33bf1605df235bd33113392
LimHyeji <ihj028003@gmail.com>
2024-01-29 21:24:26 +0900

    Feat: 수학 풀이

---
+++ csharp_algo/csharp_algo/baekjoon/boj_19532_수학은비대면강의입니다.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+
+namespace csharp_algo.baekjoon
+{
+    class boj_19532_수학은비대면강의입니다
+    {
+        
+        public static void Main()
+        {
+            string[] input = ReadLine().Split(' ');
+
+            int[] arr = new int[6];
+            for(int i = 0; i < 6; i++)
+            {
+                arr[i] = int.Parse(input[i]);
+            }
+
+            for(int i = -999; i <= 999; i++)
+            {
+                for(int j = -999; j <= 999; j++)
+                {
+                    if (arr[0] * i + arr[1] * j == arr[2] && arr[3] * i + arr[4] * j == arr[5])
+                    {
+                        WriteLine(i + " " + j);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
