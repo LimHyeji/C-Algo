3c80fdd1317fe3fd5b22996675c932292a7f1c5e
LimHyeji <ihj028003@gmail.com>
2024-02-01 21:47:46 +0900

    Feat: HashSet 풀이

---
+++ csharp_algo/csharp_algo/baekjoon/boj_14425_문자열집합.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+
+namespace csharp_algo.baekjoon
+{
+    class boj_14425_문자열집합
+    {
+        public static void Main()
+        {
+            string[] input = ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+
+            HashSet<string> set = new HashSet<string>();
+            for(int i = 0; i < N; i++)
+            {
+                string word = ReadLine();
+                set.Add(word);
+            }
+
+            int result = 0;
+            for(int i = 0; i < M; i++)
+            {
+                string word = ReadLine();
+                if (set.Contains(word)) result++;
+            }
+
+            WriteLine(result);
+        }
+    }
+}
